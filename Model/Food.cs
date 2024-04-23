e621853dcd77451636374720f15e92979ebab56d
Dhruvin Jariwala <dhruvinjariwala422002@gmail.com>
2024-04-23 10:20:33 +0530

    Add project files.

---
+++ Model/Food.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestApiCore.Model
+{
+    public partial class Food
+    {
+        public int Foodid { get; set; }
+        public string Foodname { get; set; } = null!;
+        public int Foodquantity { get; set; }
+        public int Foodprice { get; set; }
+    }
+}
